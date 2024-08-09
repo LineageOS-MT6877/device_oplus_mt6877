641b1b7dce759f8dd93d4ba82e13a3a717f83346
erfanoabdi <erfangplus@gmail.com>
2024-08-09 10:00:16 +0530

    mt6877:Uprev Wi-Fi service to 1.6

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # kernel
-git clone https://github.com/oneplus-mediatek-dev/Kernel_oneplus_ivan.git -b main kernel/oplus/mt6877
+git clone https://github.com/notganesh/Kernel_oneplus_ivan.git -b main kernel/oplus/mt6877
 
 # device tree
-git clone https://github.com/oneplus-mediatek-dev/device_oneplus_ivan.git -b main device/oplus/ivan
+git clone https://github.com/notganesh/android_device_oneplus_ivan.git -b main device/oplus/ivan
 
 # Vendor
-git clone https://github.com/oneplus-mediatek-dev/vendor_oplus_ivan.git vendor/oplus/ivan
+git clone https://github.com/notganesh/vendor_oplus_ivan.git vendor/oplus/ivan
 
 # Hardware
-git clone -b lineage-20 https://github.com/oneplus-mediatek-dev/android_hardware_mediatek.git hardware/mediatek
-git clone -b lineage-20 https://github.com/oneplus-mediatek-dev/android_hardware_mediatek_wlan.git hardware/mediatek/wlan
-git clone -b lineage-20 https://github.com/oneplus-mediatek-dev/android_hardware_oplus.git hardware/oplus
+git clone -b lineage-20 https://github.com/notganesh/android_hardware_mediatek.git hardware/mediatek
+git clone -b lineage-20 https://github.com/notganesh/android_hardware_mediatek_wlan.git hardware/mediatek/wlan
+git clone -b lineage-20 https://github.com/notganesh/android_hardware_oplus.git hardware/oplus
 
 # Sepolicy
 git clone -b lineage-20 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 
 # clang
-git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
+git clone --depth=1 https://github.com/Notganesh/android_prebuilts_clang_host_linux.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
