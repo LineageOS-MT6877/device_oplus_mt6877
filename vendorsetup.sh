b77e9ff81cba5fb8bf4daa723494ce82ab6021c4
Notganesh <chimeports@gmail.com>
2024-10-11 16:53:16 +0000

    ivan:Inherit common.mk

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,15 +4,15 @@
 git clone https://github.com/notganesh/Kernel_oneplus_ivan.git -b main kernel/oplus/mt6877
 
 # device tree
-git clone https://github.com/notganesh/android_device_oneplus_ivan.git -b main device/oplus/ivan
+git clone https://github.com/LineageOS-MT6877/device_oneplus_ivan.git device/oplus/ivan
 
 # Vendor
-git clone https://github.com/notganesh/vendor_oplus_ivan.git vendor/oplus/ivan
+git clone https://github.com/LineageOS-MT6877/vendor_oplus_ivan.git vendor/oplus/ivan
 
 # Hardware
-git clone -b lineage-20 https://github.com/notganesh/android_hardware_mediatek.git hardware/mediatek
+git clone -b lineage-20 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
 git clone -b lineage-20 https://github.com/notganesh/android_hardware_mediatek_wlan.git hardware/mediatek/wlan
-git clone -b lineage-20 https://github.com/notganesh/android_hardware_oplus.git hardware/oplus
+git clone -b lineage-20 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 
 # Sepolicy
 git clone -b lineage-20 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
