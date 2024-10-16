da8d9c9e9784aa5d11bbbdce9f974dfb913d1007
Notganesh <chimeports@gmail.com>
2024-10-16 16:14:25 +0530

    mt6877:Currently not required for ivan *Saving For the Future

--- common.mk
+++ common.mk
@@ -16,10 +16,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-# Device-specific background service
-PRODUCT_PACKAGES += \
-    OssiDeviceService
-
 # Always use GPU for screen compositing
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_hwc_overlays=1
