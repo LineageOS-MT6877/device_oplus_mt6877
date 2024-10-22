3096d67e6a6b982a7ace04919f6727ba4c31f420
Notganesh <chimeports@gmail.com>
2024-10-22 17:37:41 +0000

    mt6877:track repos from los

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+rm -rf hardware/oplus
+rm -rf hardware/mediatek
+
+git clone https://github.com/LineageOS-MT6877/device_oneplus_ivan.git device/oplus/ivan
+
 git clone https://github.com/Notganesh/kernel_oneplus_ivan.git kernel/oplus/mt6877
 
-git clone https://gitlab.com/notganesh/vendor_oneplus_ivan.git vendor/oplus/ivan
+git clone -b lineage-20 https://github.com/Notganesh/vendor_oneplus_ivan.git vendor/oplus/ivan
 
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 
